6640bc0360d59544b5e525529d6cdae80d9865dd
Turakulov Utkir <utkir.turakulov@simbirsoft.com>
2018-10-04 17:09:06 +0400

    Добавлена левая границы отрисовки

--- Маятник/Pendulum.cs
+++ Маятник/Pendulum.cs
@@ -9,7 +9,9 @@ using System.Windows.Forms;
 
 namespace Маятник
 {
-
+    /// <summary>
+    /// Объект маятника реализуемый по паттерну синглтон
+    /// </summary>
     class Pendulum
     {
         private static Pendulum _instance;
@@ -22,6 +24,8 @@ namespace Маятник
 
         /// <summary>
         /// Определяет направление двичение маятника
+        /// CounterClockwise - против часовой стрелки
+        /// Clockwise - по часовой стрелке
         /// </summary>
         private enum Direction 
         {
@@ -34,6 +38,10 @@ namespace Маятник
         {
         }
 
+        /// <summary>
+        /// Возвращает инстанс класса
+        /// </summary>
+        /// <returns></returns>
         public static Pendulum GetInstance()
         {
             if (_instance == null)
@@ -49,6 +57,12 @@ namespace Маятник
             return _instance;
         }
 
+        /// <summary>
+        /// Запускает задачу на отрисовку маятника
+        /// </summary>
+        /// <param name="form">Форма на которой отрисовать маятник</param>
+        /// <param name="token">Токен для прерываия задачи</param>
+        /// <returns>Task</returns>
         public Task Draw(Form form, CancellationToken token)
         {
             return new Task(() =>
@@ -63,20 +77,37 @@ namespace Маятник
                 int counter = 100;
                 double x = -1;
                 double y = y0;
+                double leftBorderX = -1 ;
+
                 double angle = 10;
                 double speed = 6;
 
                 while (!token.IsCancellationRequested)
                 {
-                    if (x < 0)
+                    if (_actiualDirection == (int)Direction.Clockwise)
                     {
-                        x = x0 + radius * Math.Cos(DegreeToRadian(angle * speed));
+                        if (x < 0)
+                        {
+                            x = x0 + radius * Math.Cos(DegreeToRadian(angle * speed));
+                            leftBorderX =x -( 2 *radius);
+                        }
+                        else
+                        {
+                            x = x0 + radius * Math.Cos(DegreeToRadian(angle * speed));
+                            y = y0 + radius * Math.Sin(DegreeToRadian(angle * speed));
+                        }
+
+                        if (x == leftBorderX)
+                        {
+                            _actiualDirection = (int)Direction.CounterClockwise;
+                        }
+                        
                     }
                     else
                     {
-                        x = x0 + radius * Math.Cos(DegreeToRadian(angle * speed));
-                        y = y0 + radius * Math.Sin(DegreeToRadian(angle * speed));
+                        
                     }
+                    
                     graphics.DrawLine(pen, new Point(x0, y0), new Point((int)x, (int)y));
                     angle++;
                     counter--;
